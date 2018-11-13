d469c19852eb9b499e284036078d55fc57bbb7d2
Tanuj Gaikwad <tanujgaikwad@garnetvalley.org>
2018-11-13 14:38:09 -0500

    11/13/2018

---
+++ src/Chapter6Projects/Number7a.java
@@ -0,0 +1,20 @@
+package Chapter6Projects;
+
+public class Number7a
+{
+	public static void main(String[] args)
+	{
+		int starMax = 10;
+		for (int j = 0; j < 10; j++)
+		{
+			for (int i = 0; i < starMax; i++)
+			{
+					System.out.print("*");
+			}
+			starMax--;
+			System.out.println();
+		}
+
+	}
+
+}
