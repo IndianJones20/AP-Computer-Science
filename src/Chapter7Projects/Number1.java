6dbdbbf03559eaaa8d481e085023c37b276daf01
IndianJones20 <44714790+IndianJones20@users.noreply.github.com>
2018-12-01 10:49:28 -0500

    Update Number1.java

--- src/Chapter7Projects/Number1.java
+++ src/Chapter7Projects/Number1.java
@@ -9,26 +9,28 @@ public class Number1
 		Number1Methods acct2 = new Number1Methods();
 		Number1Methods acct3 = new Number1Methods();
 
-		while (acct1.login("Jane Smith", 68578))
-		{
-
-		}
 		acct1.createAccount("Ted Murphy");
+		acct1.printAcctNum();
 		acct2.createAccount("Jane Smith");
-		
-		acct1.deposit(25.85);
-		acct2.deposit(235.63);
-		
-		double smithBalance = acct2.deposit(500.00);
-		System.out.println("Smith balance after deposit: " + smithBalance);
+		acct2.printAcctNum();
 
-		System.out.println("Smith balance after withdrawal: " + acct2.withdraw(430.75, 1.50));
-		acct1.addInterest();
-		acct2.addInterest();
-		acct3.addInterest();
+	while(true)
+	{
+	
+		acct1.login(“Ted Murphy”, acct1.printAcctNum())
+//		acct1.deposit(25.85);
+//		acct2.deposit(235.63);
+		
+//		double smithBalance = acct2.deposit(500.00);
+//		System.out.println("Smith balance after deposit: " + smithBalance);
 
-		System.out.println();
-		System.out.println(acct1);
+//		System.out.println("Smith balance after withdrawal: " + acct2.withdraw(430.75, 1.50));
+//		acct1.addInterest();
+//		acct2.addInterest();
+//		acct3.addInterest();
+
+//		System.out.println();
+//		System.out.println(acct1);
 //		System.out.println(acct2);
 //		System.out.println(acct3);
 
