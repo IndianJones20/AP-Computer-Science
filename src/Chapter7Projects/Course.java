380ba86032bfe4530a978d1bb66543d9ee678d67
IndianJones20 <44714790+IndianJones20@users.noreply.github.com>
2018-12-01 12:03:55 -0500

    Create Course.java

---
+++ src/Chapter7Projects/Course.java
@@ -0,0 +1,27 @@
+package Chapter7Projects;
+
+public class Course()
+{
+	ArrayList<String> studentsTakingCourse = ne ArrayList<String>();
+	private String course;
+	public Course(String courseName)
+	{
+		course = courseName;
+	}
+	
+	
+	public void addStudent(Student student)
+	{
+		
+	}d
+	
+	public int average()
+	{
+		int counter = 0;
+		while(counter < studentsTakingCourse.size())
+		{
+			studentsTakingCourse.
+		}
+	}
+	
+}
