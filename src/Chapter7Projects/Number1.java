25aec4d75c1084fb4d28d4b2298cedec3c5f1714
Tanuj Gaikwad <tanujgaikwad@garnetvalley.org>
2018-12-01 19:22:56 -0500

    12/01/2018

--- src/Chapter7Projects/Number1.java
+++ src/Chapter7Projects/Number1.java
@@ -14,26 +14,28 @@ public class Number1
 		acct2.createAccount("Jane Smith");
 		acct2.printAcctNum();
 
-	while(true)
-	{
-	
-		acct1.login(“Ted Murphy”, acct1.printAcctNum())
-//		acct1.deposit(25.85);
-//		acct2.deposit(235.63);
-		
-//		double smithBalance = acct2.deposit(500.00);
-//		System.out.println("Smith balance after deposit: " + smithBalance);
+		while (true)
+		{
 
-//		System.out.println("Smith balance after withdrawal: " + acct2.withdraw(430.75, 1.50));
-//		acct1.addInterest();
-//		acct2.addInterest();
-//		acct3.addInterest();
+			acct1.login("Ted Murphy", acct1.printAcctNum());
+			// acct1.deposit(25.85);
+			// acct2.deposit(235.63);
 
-//		System.out.println();
-//		System.out.println(acct1);
-//		System.out.println(acct2);
-//		System.out.println(acct3);
+			// double smithBalance = acct2.deposit(500.00);
+			// System.out.println("Smith balance after deposit: " + smithBalance);
+
+			// System.out.println("Smith balance after withdrawal: " +
+			// acct2.withdraw(430.75, 1.50));
+			// acct1.addInterest();
+			// acct2.addInterest();
+			// acct3.addInterest();
+
+			// System.out.println();
+			// System.out.println(acct1);
+			// System.out.println(acct2);
+			// System.out.println(acct3);
+
+		}
 
 	}
-
 }
