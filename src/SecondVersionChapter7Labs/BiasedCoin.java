b2a9e38ecf33371ff30f2519b2484d883ca610ea
IndianJones20 <tanuj.gaikwad@gmail.com>
2018-12-05 12:05:00 -0500

    12/05/2018

---
+++ src/SecondVersionChapter7Labs/BiasedCoin.java
@@ -0,0 +1,50 @@
+package SecondVersionChapter7Labs;
+
+public class BiasedCoin
+{
+	private final int HEADS = 0;
+	private final int TAILS = 1;
+	private double bias;
+
+	private double coinFace;
+
+	public BiasedCoin()
+	{
+		bias = 0.5;
+		flip();
+	}
+
+	public BiasedCoin(double Yeetbias)
+	{
+		bias = Yeetbias;
+		flip();
+	}
+
+	public void flip()
+	{
+		coinFace = (Math.random());
+		if (coinFace < bias)
+		{
+			coinFace = 0;
+		}
+
+		else
+			coinFace = 1;
+	}
+
+	public boolean isHeads()
+	{
+		return (coinFace == HEADS);
+	}
+
+	public String toString()
+	{
+		String faceName;
+		if (coinFace == HEADS)
+			faceName = "Heads";
+		else
+			faceName = "Tails";
+
+		return faceName;
+	}
+}
