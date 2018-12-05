44747e05885e2a98ab7742a8ab6e18ce81edeb89
IndianJones20 <tanuj.gaikwad@gmail.com>
2018-12-05 17:47:40 -0500

    12/05/2018

--- src/Chapter7Lab/compare3.java
+++ src/Chapter7Lab/compare3.java
@@ -1,15 +1,15 @@
 package Chapter7Lab;
 
-public class Compare3
+public class compare3
 {
-	public static Comparable largest(Comparable yeet, Comparable yeet2, Comparable yeet3)
+	public static Comparable largest(Comparable yeet1, Comparable yeet2, Comparable yeet3)
 	{
-		if (yeet.compareTo(yeet2) > 0 && yeet.compareTo(yeet3) > 0)
+		if (yeet1.compareTo(yeet2) > 0 && yeet1.compareTo(yeet3) > 0)
 		{
-			return yeet;
+			return yeet1;
 		}
 
-		else if (yeet2.compareTo(yeet3) > 0 && yeet2.compareTo(yeet) > 0)
+		else if (yeet2.compareTo(yeet3) > 0 && yeet2.compareTo(yeet1) > 0)
 		{
 			return yeet2;
 		}
