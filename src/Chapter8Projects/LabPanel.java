fd9c8926704f1a16ada4d3b40974f5a5dd26730a
IndianJones20 <tanuj.gaikwad@gmail.com>
2018-12-16 16:14:26 -0500

    12/16/2018

---
+++ src/Chapter8Projects/LabPanel.java
@@ -0,0 +1,17 @@
+package Chapter8Projects;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+public class LabPanel extends JPanel implements Runnable
+{
+	Image img;
+
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+	}
+}
